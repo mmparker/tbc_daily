219707d01319defbcf877c19589015eb594d1488
Matt Parker <mattmparker@gmail.com>
2013-07-03 16:14:59 -0600

    Convert to jQuery UI tab navigation.

--- generate_report.r
+++ generate_report.r
@@ -13,8 +13,10 @@ library(markdown)
 # Knit it
 knit("daily_status.rmd")
 
-markdownToHTML(file = "daily_status.md",
-               output = "daily_status.html",
-               stylesheet = file.path("..", "css", "tb_report.css"))
+# markdownToHTML(file = "daily_status.md",
+#                output = "daily_status.html",
+#                stylesheet = file.path("..", "css", "tb_report.css"))
 
+
+system("pandoc daily_status.md -o daily_status.html")
 
